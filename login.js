d999a8cc9dbe13da733d136027fd68ece243e7db
amancoach001-source <amancoach001@gmail.com>
2026-09-16 02:41:23 +0530

    Redirect to index.html after login

--- login.js
+++ login.js
@@ -128,6 +128,6 @@ loginForm.addEventListener("submit", function (e) {
 
     // AI Study Assistant खोलना
     setTimeout(() => {
-        window.location.href = "home.html";
+        window.location.href = "index.html";
     }, 1000);
 });
